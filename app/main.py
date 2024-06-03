b71e415bb7e951fe60ea61ddb3660a3d4936b045
Sujan Midatani <sujanmidatani7@gmail.com>
2024-06-03 22:46:51 +0530

    added middle ware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
+from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"]
+)
 
 
 
