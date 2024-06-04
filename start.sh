77804340cae30b2fd5664620ed6bd9b05038534e
Sujan Midatani <sujanmidatani7@gmail.com>
2024-06-04 18:41:10 +0530

    modified docker file

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 
 echo "Starting FastAPI server"
-exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8001
 echo "FastAPI server started"
